b9f68670f236152f6466311c003d1be4077ff5dc
Ratikanta Behera <136294827+ratikanta-25@users.noreply.github.com>
2025-07-25 22:01:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("bookingForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Your request has been submitted successfully!");
+  // TODO: Integrate with backend or Firebase to store requests
+});
